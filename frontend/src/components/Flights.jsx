8c268ec99e6a186759634ad5260a2bcbd86bee55
nostalgic-math-student <josue.rojas@ciencias.unam.mx>
2023-09-21 21:54:13 -0600

    Levenshtein search engine implemented

--- frontend/src/components/Flights.jsx
+++ frontend/src/components/Flights.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import Table from './MainTable.jsx';
+import React, { useState } from 'react'
+import List from './List.js'
+
 
 const Flights = () => {
+
+  const [queryInput, setQueryInput] = useState("");
+  const IATA = ["Mexico", "Hello"]
+
+let inputHandler = (event) => {
+  //convert input text to lower case
+  var lowerCase = event.target.value;
+  setQueryInput(lowerCase);
+};
+
+
   return (
 <div className="md:container mx-auto">
-  <Table/>
+<div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" value={queryInput.toLowerCase()} onChange={inputHandler}/>
+      <List input={queryInput}></List>
+    </div>
 </div>
   )
 }
---
+++ frontend/src/components/List.js
@@ -0,0 +1,96 @@
+import data from './ListData.json'
+import { React, useState, useEffect, useMemo } from 'react'
+
+function List(props) {
+
+    // Implementacion extraida de https://masoudx.medium.com/sorting-words-by-similarity-in-typescript-a-guide-to-use-levenshtein-distance-algorithm-f6b4f3b57008
+    const levenshteinDistance = (a, b) => {
+
+        // Create a 2D array to store the distances
+        let distances = new Array(a.length + 1);
+        for (let i = 0; i <= a.length; i++) {
+            distances[i] = new Array(b.length + 1);
+        }
+
+
+        // Initialize the first row and column
+        for (let i = 0; i <= a.length; i++) {
+            distances[i][0] = i;
+        }
+        for (let j = 0; j <= b.length; j++) {
+            distances[0][j] = j;
+        }
+
+        // Fill in the rest of the array
+        for (let i = 1; i <= a.length; i++) {
+            for (let j = 1; j <= b.length; j++) {
+                if (a[i - 1] === b[j - 1]) {
+                    distances[i][j] = distances[i - 1][j - 1];
+                } else {
+                    distances[i][j] = Math.min(distances[i - 1][j], distances[i][j - 1], distances[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+
+        // Return the final distance
+        return distances[a.length][b.length];
+    }
+
+    const sortBySimilarity = (words, query) => {
+        // Create an array of objects to store the words and their distances
+        let wordDistances = words.map(word => ({
+            word: word,
+            distance: levenshteinDistance(word.toLowerCase(), query.toLowerCase())
+        }));
+
+        // Sort the array by distance
+        wordDistances.sort((a, b) => a.distance - b.distance);
+
+        // Return the sorted list of words
+        return wordDistances.map(wd => wd.word);
+    }
+
+    const sortedData = useMemo(() => {
+        // Create a copy of the data array before sorting
+        const newData = [...data];
+
+        // Sort the data array based on Levenshtein distance for both name and iataCode
+        newData.sort((a, b) => {
+            const distanceNameA = levenshteinDistance(a.text.toLowerCase(), props.input);
+            const distanceNameB = levenshteinDistance(b.text.toLowerCase(), props.input);
+            
+            const distanceIataCodeA = levenshteinDistance(a.iataCode.toLowerCase(), props.input);
+            const distanceIataCodeB = levenshteinDistance(b.iataCode.toLowerCase(), props.input);
+
+            // Use the minimum distance between name and iataCode
+            const minDistanceA = Math.min(distanceNameA, distanceIataCodeA);
+            const minDistanceB = Math.min(distanceNameB, distanceIataCodeB);
+
+            return minDistanceA - minDistanceB;
+        });
+        return newData;
+    }, [props.input]);
+    
+    const filteredData = sortedData.filter((event) => {
+        if (props.input === '') {
+            return true;
+        }
+        else {
+            
+            const matchingName = sortBySimilarity([event.text], props.input)
+            const matchingIataCode = sortBySimilarity([event.iataCode], props.input)
+            return matchingName.length > 0 || matchingIataCode > 0
+        }
+    }).slice(0,3)
+    
+
+    return (
+        <ul>
+            {filteredData.map((item, index) => (
+                <li key={item.id}>{item.text} ({item.iataCode})</li>
+            ))}
+        </ul>
+    )
+}
+
+export default List
